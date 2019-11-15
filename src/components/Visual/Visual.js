c2775bfa1dfee3f363042d60fa01d925b8ab60aa
optimiJH <jackwu851216@gmail.com>
2019-11-14 20:06:21 -0600

    Add AccessToken -> tracks, tracksArray -> Tracks Audio features into State
    Add AccessToken -> tracks, tracksArray -> Tracks Audio features into State

--- src/components/Visual/Visual.js
+++ src/components/Visual/Visual.js
@@ -12,26 +12,24 @@ class Visual extends React.Component {
 
   constructor() {
     super();
-    console.log("hit");
     this.state = {
       response: null,
-      accessToken :null,
-      playlistId :null,
-      tracks:null
+      index: 0,
+      limit: 0,
+      songs: {},
+      accessToken: null, 
+      playlistId: null,
+      tracks: null,     //List of songName, artist, album
+      tracksArr:null,   //List of songId (Used for audio features API)
+      tracksAudioFeatures:null  //List of songs audio features
     };
   }
 
-  parsePlaylistId(){
-
-    let str = window.location.search;
-    let res = str.split("/");
-    // console.log(res[5]);
-    let playlistid = res[5].split("?");
-    this.setState({playlistId:playlistid[0]});
-  }
-
-
-  spotifyGetAccessToken() {
+  componentDidMount() {
+    //Flow :
+    //AccessToken -> tracks, tracksArray -> Tracks Audio features
+    //Use Javascript Promise object to concatenate response and step by step
+    //store result into State
     const cors_url = 'https://cors-anywhere.herokuapp.com/';
     const api_url = 'https://accounts.spotify.com/api/token';
     const request_url = cors_url + api_url;
@@ -42,7 +40,6 @@ class Visual extends React.Component {
       grant_type: 'client_credentials',
       refresh_token: 'BQDLgWFfNca-UOxmt8oBYyorMVhRI0Na2sBhk0ieCApurG4_V_x52gwohg3EXiwdMjtsTnoENPsrKXqa526Cr6VzYX5B8eWbJaemyhgPNksXOix8vo4xMdyIapi8jXwwams1x8mqaT6AGEZlAAJloW4wIs_IgxQhear7He-Qx7tfjqb62Wk6TSMv'
     };
-
     axios.post(request_url, qs.stringify(bodyParameters), {
       headers: {
         'Authorization': basicAuth,
@@ -51,115 +48,83 @@ class Visual extends React.Component {
     }).then((response) => {
 
       this.setState({accessToken:response.data.access_token});
+      const { location, history } = this.props;
+      const path = location.pathname;
+      const urlParams = new URLSearchParams(window.location.search);
+      const playlistId = urlParams.get('pl');
+      //Save AccessToken into State
+      this.setState({playlistId:playlistId});
+      return playlistId;
 
+    }).then((playlistId)=>{
+
+        let SpotifyWebApi = require('spotify-web-api-js');
+        let spotifyApi = new SpotifyWebApi({
+          clientId: 'd83740bfd39e48cd9e1cee53425b375b',
+          clientSecret: 'eeb7bd011f2d41a0a0284a37a296cbf6'
+        });
+        spotifyApi.setAccessToken(this.state.accessToken);
+        spotifyApi.getPlaylistTracks(playlistId)
+        .then((data) => {
+
+          let tracks = [];
+          let tracksArr = [];
+
+          data.items.forEach(function (arrayItem) {
+              let track = {};
+              track.songName = arrayItem.track.name;
+              track.id = arrayItem.track.id;
+              track.artist = arrayItem.track.artists;
+              track.album = arrayItem.track.album;
+              tracks.push(track);
+              tracksArr.push(track.id);
+
+          });
+          //Save tracks, tracksArr into State
+          this.setState({tracks : tracks});
+          this.setState({tracksArr : tracksArr});
+          return tracksArr;
+
+        }).then((tracksArr)=>{
+          let SpotifyWebApi = require('spotify-web-api-js');
+          let spotifyApi = new SpotifyWebApi({
+            clientId: 'd83740bfd39e48cd9e1cee53425b375b',
+            clientSecret: 'eeb7bd011f2d41a0a0284a37a296cbf6'
+          });
+          spotifyApi.setAccessToken(this.state.accessToken);
+          spotifyApi.getAudioFeaturesForTracks(tracksArr).then(
+            (data)=> {
+              //Save tracks Audio Features into State
+              this.setState({tracksAudioFeatures:data.audio_features})
+            },
+            function(err) {
+              console.error(err);
+            });
+        });
     }).catch((error) => {
       console.log(error);
     });
-  }
-
-  onComplete(input){
-    console.log(" On completes");
-    // this.setState({tracks : input});
-  }
-
-
-  spotifyGetTracksViaPlaylist(whenDone){
-    let SpotifyWebApi = require('spotify-web-api-js');
-
-    let spotifyApi = new SpotifyWebApi({
-      clientId: 'd83740bfd39e48cd9e1cee53425b375b',
-      clientSecret: 'eeb7bd011f2d41a0a0284a37a296cbf6'
-    });
-    // spotifyApi.setAccessToken('BQBr8zoFUITS3oe7Ex1oCo4BOb-dW5J0_fCVf-t3jVlBGEKLEqs2g1JkNdd-qDcCvI4cpoVxF8m5RCZncL8');
-    // spotifyApi.getPlaylistTracks('5ORlAqcF4dhRbEgs7ohRn8')
-
-    spotifyApi.setAccessToken(this.state.accessToken);
-    spotifyApi.getPlaylistTracks(this.state.playlistId)
-    .then((data) => {
-
-      let tracks = [];
-
-      console.log('Tracks: ', data.items);
-
-
-
-      data.items.forEach(function (arrayItem) {
-          var x = arrayItem.track;
-          // console.log(x);
-          let track = {}
-          track.songName = arrayItem.track.name;
-          track.id = arrayItem.track.id;
-          track.artist = arrayItem.track.artists;
-          track.album = arrayItem.track.album;
-          tracks.push(track);
-          // return tracks;
-
-      });
-      console.log(tracks);
-      this.setState({tracks : tracks});
-      whenDone(tracks);
-    }, function(err) {
-      console.error(err);
-    });
-    // console.log(tracks);
-    // this.setState({tracks : "AAA"});
-  }
-
-  spotifyGetAudioFeatures(){
-    let SpotifyWebApi = require('spotify-web-api-js');
-
-    let spotifyApi = new SpotifyWebApi({
-      clientId: 'd83740bfd39e48cd9e1cee53425b375b',
-      clientSecret: 'eeb7bd011f2d41a0a0284a37a296cbf6'
-    });
-    spotifyApi.setAccessToken(this.state.accessToken);
-    // spotifyApi.setAccessToken('BQCcvlRQpsbWfBUOfFd6sUWGIz5k3s1rcev6zO4zpjDyoYwJLvM2Ma5w7L3QQD_HlztLs_NZ8c6VI6Um0N4');
-    var array= ['4JpKVNYnVcJ8tuMKjAj50A','2NRANZE9UCmPAS5XVbXL40','24JygzOLM0EmRQeGtFcIcG']
-    spotifyApi.getAudioFeaturesForTracks(array).then(
-      function(data) {
-        console.log('Audio Features for tracks : ', data.audio_features);
-      },
-      function(err) {
-        console.error(err);
-      }
-      );
-  }
-
-  componentDidMount() {
-    this.spotifyGetAccessToken() ;
-    this.parsePlaylistId();
   }
 
   render() {
     const { location, history } = this.props;
     const path = location.pathname;
     const urlParams = new URLSearchParams(window.location.search);
-
     const playlistId = urlParams.get('pl');
     const playlistUrl = 'https://open.spotify.com/embed/playlist/' + playlistId;
     const songs = dataProvider.createRandomSongs();
-    dataProvider.spotifyPost();
-    //dataProvider.spotifyGet();
-    dataProvider.spotifyGetPlaylistTrackCount(playlistId);
-    dataProvider.spotifyGetPlaylistPage(playlistId);
 
-
-    if(this.state.accessToken != null && this.state.playlistId!=null){
-      // this.spotifyGetAudioFeatures();
-      console.log("Yes token: "+this.state.accessToken );
-      console.log("playlistId: "+this.state.playlistId );
-
-      // Let's get audioFeatures
-      this.spotifyGetTracksViaPlaylist(this.onComplete);
-
-      if(this.state.tracks!=null){
-        console.log("Here:"+this.state.tracks);
-      }
-      this.spotifyGetAudioFeatures();
-
-    }else{
-      console.log("No token");
+    //Ben's Part
+    if(this.state.accessToken!=null && this.state.playlistId!=null){
+      dataProvider.spotifyGetPlaylistTrackCount(this.state.accessToken,this.state.playlistId);
+      dataProvider.spotifyGetPlaylistPage(this.state.accessToken,this.state.playlistId);
     }
+
+      //For test
+      console.log("playlistId",this.state.playlistId);
+      console.log("Tracks in Render: ", this.state.tracks);
+      console.log("TracksArray: ", this.state.tracksArr);
+      console.log("TracksAudioFeatures: ", this.state.tracksAudioFeatures);
 
     return (
       <ThemeProvider theme={theme}>
--- src/helper/dataProvider.js
+++ src/helper/dataProvider.js
@@ -4,24 +4,25 @@ import qs from 'qs';
 
 //FIXME: we should really be saving the access token somewhere instead of hardcoding it
 const dataProvider = {
-  createRandomSongs: () => {
-    let songs = {};
-    let songNum = 30;
 
-    for (let i = 0; i < songNum; i++) {
-      songs[i] = {
-        songName: 'random' + (i + 1),
-        position: [
-          Math.random() * 100,
-          Math.random() * 100
-        ]
-      };
-    }
+ createRandomSongs: () => {
+   let songs = {};
+   let songNum = 30;
 
-    return songs;
-  },
+   for (let i = 0; i < songNum; i++) {
+     songs[i] = {
+       songName: 'random' + (i + 1),
+       position: [
+         Math.random() * 100,
+         Math.random() * 100
+       ]
+     };
+   }
+
+   return songs;
+ },
   spotifyGetSongFeatures: () => {
-    const accessToken = 'BQDeKn3tKSnI6yJsgQwC-eOmhDYpsWiSoaQNYqLecifa43jvOV6FVc4LXT_laQsyih10KK0whkVfdWNzWUo';
+    const accessToken = 'BQCJ_Qbe4MzEAB0Y8kXdyR-a0vVkS8fHWF1kFagSXALntNq0v8aLjTR1W6lS8ff0FyoNSm5EFtL2-m-UteM';
     axios.defaults.headers.common = {
       'Authorization': 'Bearer ' + accessToken
     };
@@ -35,34 +36,12 @@ const dataProvider = {
       console.log(response.data);
     });
   },
-  spotifyPost: () => {
-    const cors_url = 'https://cors-anywhere.herokuapp.com/';
-    const api_url = 'https://accounts.spotify.com/api/token';
-    const request_url = cors_url + api_url;
-    const clientId = '436d61cb446a48b0a9867aed113380c4';
-    const clientSecret = '647ae3f395474d74bde32870ccf955e7';
-    const basicAuth = 'Basic ' + btoa(clientId + ':' + clientSecret);
-    const bodyParameters = {
-      grant_type: 'client_credentials',
-      refresh_token: 'BQDLgWFfNca-UOxmt8oBYyorMVhRI0Na2sBhk0ieCApurG4_V_x52gwohg3EXiwdMjtsTnoENPsrKXqa526Cr6VzYX5B8eWbJaemyhgPNksXOix8vo4xMdyIapi8jXwwams1x8mqaT6AGEZlAAJloW4wIs_IgxQhear7He-Qx7tfjqb62Wk6TSMv'
-    };
 
-    axios.post(request_url, qs.stringify(bodyParameters), {
-      headers: {
-        'Authorization': basicAuth,
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'X-Requested-With': 'XMLHttpRequest'
-      }
-    }).then((response) => {
-      console.log(response);
-    }).catch((error) => {
-      console.log(error);
-    });
-  },
   //Retrieves the total number of songs that we're going to need to grab.
   //FIXME: how do you store this data in react?
-  spotifyGetPlaylistTrackCount: (playlistId) => {
-    const accessToken = 'BQDeKn3tKSnI6yJsgQwC-eOmhDYpsWiSoaQNYqLecifa43jvOV6FVc4LXT_laQsyih10KK0whkVfdWNzWUo';
+  //RESPONSE: if you want to store data into State, these two method might need to be moved to VisuaL.js
+  spotifyGetPlaylistTrackCount: (accessToken,playlistId) => {
+    // const accessToken = 'BQCfKhrq3XYcgVosRQwoLK1A6quNiA1CdC06tV7DzWX_dbeHkdekNbNagY6VSbTVSkICIQEkHz_lhlkBV3o';
     axios.defaults.headers.common = {
       'Authorization': 'Bearer ' + accessToken
     };
@@ -77,8 +56,8 @@ const dataProvider = {
       //console.log(LogisticsTracker.limit);
     });
   },
-  spotifyGetPlaylistPage: (playlistId) => {
-    const accessToken = 'BQDeKn3tKSnI6yJsgQwC-eOmhDYpsWiSoaQNYqLecifa43jvOV6FVc4LXT_laQsyih10KK0whkVfdWNzWUo';
+  spotifyGetPlaylistPage: (accessToken,playlistId) => {
+    // const accessToken = 'BQCfKhrq3XYcgVosRQwoLK1A6quNiA1CdC06tV7DzWX_dbeHkdekNbNagY6VSbTVSkICIQEkHz_lhlkBV3o';
     axios.defaults.headers.common = {
       'Authorization': 'Bearer ' + accessToken
     };
